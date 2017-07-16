1d1238225326c062aa30bcceddf9a98bd2a7d733
Bohdan Turkynewych <tb0hdan@gmail.com>
2017-07-16 22:05:14 +0300

    humanize + separate plugins file

--- src/torpedobot/stats.go
+++ src/torpedobot/stats.go
@@ -4,64 +4,32 @@ import (
 	"fmt"
 	"runtime"
 	"time"
+	"strconv"
 	"github.com/tb0hdan/torpedo_registry"
+	humanize "github.com/dustin/go-humanize"
 )
-
-func FormatTDiff(ts int64) (int64, int64, int64, int64) {
-	m, s := ts/60, ts%60
-	h, m := m/60, m%60
-	d, h := h/24, h%24
-	return d, h, m, s
-}
-
-func CalculateMessageRate(tdiff, msgcount int64) (result string) {
-	var value int64
-	pairs := make(map[string]int64)
-	pairs["s"] = 0
-	pairs["m"] = 60
-	pairs["h"] = 3600
-	pairs["d"] = 86400
-	if msgcount == 0 {
-		result = "0/s"
-		return
-	}
-	// BUGGY!
-	for key := range pairs {
-		if pairs[key] == 0 {
-			value = msgcount / tdiff
-		} else {
-			value = msgcount / (tdiff / pairs[key])
-		}
-		if value > 0 {
-			result = fmt.Sprintf("%v/%s", value, key)
-			break
-		}
-	}
-	return
-}
 
 func GetMemStats() (result string) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	result = fmt.Sprintf("\n\tAlloc = %v KB\n\tSys = %v KB\n\tNumGC = %v\n\n", m.Alloc/1024, m.Sys/1024, m.NumGC)
+	result = fmt.Sprintf("\n\tAlloc = %s\n\tSys = %s\n\tNumGC = %s\n\n", humanize.Bytes(m.Alloc), humanize.Bytes(m.Sys), humanize.Comma(int64(m.NumGC)))
 	return
 }
 
 func StatsProcessMessage(api *torpedo_registry.BotAPI, channel interface{}, incoming_message string) {
-	//api = &Dummy{}
-
-	ts := int64(time.Now().Unix()) - api.Bot.Stats.StartTimestamp
-	d, h, m, s := FormatTDiff(ts)
+	i, err := strconv.ParseInt(fmt.Sprintf("%v", api.Bot.Stats.StartTimestamp), 10, 64)
+	if err != nil {
+		panic(err)
+	}
 	message := fmt.Sprintf("Project URL: %s\n", api.Bot.Build.ProjectURL)
 	message += fmt.Sprintf("Build hash: %s\n", api.Bot.Build.Build)
 	message += fmt.Sprintf("Build date: %s\n", api.Bot.Build.BuildDate)
 	message += fmt.Sprintf("Build version: %s\n", api.Bot.Build.Version)
-	message += fmt.Sprintf("Uptime: %v day(s) %v hour(s) %v minute(s) %v second(s)\n", d, h, m, s)
-	message += fmt.Sprintf("Processed messages (session): %v\n", api.Bot.Stats.ProcessedMessages)
-	message += fmt.Sprintf("Processed messages (total): %v\n", api.Bot.Stats.ProcessedMessagesTotal)
+	message += fmt.Sprintf("Bot started: %s\n", humanize.Time(time.Unix(i, 0)))
+	message += fmt.Sprintf("Processed messages (session): %s\n", humanize.Comma(api.Bot.Stats.ProcessedMessages))
+	message += fmt.Sprintf("Processed messages (total): %s\n", humanize.Comma(api.Bot.Stats.ProcessedMessagesTotal))
 	//message += fmt.Sprintf("Message rate: %s\n", CalculateMessageRate(ts, api.Bot.Stats.ProcessedMessages))
 	message += fmt.Sprintf("Accounts connected/total: %v/%v\n", api.Bot.Stats.ConnectedAccounts, api.Bot.Stats.TotalAccounts)
 	message += fmt.Sprintf("Memory info:%s\n", GetMemStats())
 	api.Bot.PostMessage(channel, message, api)
-
 }
